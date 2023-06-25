4a33b13bfe126a8e89ba22708bd2d8a3daefee09
BekirYldrm <bekiryildirim.2508@gmail.com>
2023-06-25 21:40:24 +0300

    First Commit

---
+++ date.js
@@ -0,0 +1,21 @@
+
+
+exports.getDate = getDate ; // getDate fonksiyonunu dışa aktarıyoruz ama fonksiyonu çağırmadık ,() bunu eklemedik.
+
+function getDate () {
+
+    const today = new Date();  // sunucunun çalıştığı zamanı  today değişkenine atadık.
+
+    const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" }
+
+    return today.toLocaleDateString("en-US", options);  //today degiskenindeki tarihi options nesnesine göre biçimlendiriyor.
+}
+
+exports.getDay = function () { // isimsiz fonksiyon oluşturup dogrudan module atayabiliriz .
+    
+    const today = new Date(); 
+
+    const options = { weekday: "long" }
+
+    return today.toLocaleDateString("en-US", options); 
+}
