1fde80b3f29a0c04ddfed583275f14d140a0d555
BekirYldrm <bekiryildirim.2508@gmail.com>
2023-06-27 20:54:41 +0300

    Access to database on get and post requests

--- app.js
+++ app.js
@@ -12,66 +12,66 @@ app.use(express.static("public"))
 
 mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
 
-const itemsSchema = mongoose.Schema({ name : String})
+const itemsSchema = mongoose.Schema({ name: String });
 
-const ItemModel = mongoose.model("Item" , itemsSchema);
+const ItemModel = mongoose.model("Item", itemsSchema);
+const workItemModel = mongoose.model("WorkItem", itemsSchema);
 
-const item1 = new ItemModel({
-    name : "Sabah"
-});
-const item2 = new ItemModel({
-    name : "Öğle"
-});
-const item3 = new ItemModel({
-    name : "Akşam"
-});
+const item1 = new ItemModel({ name: "Sabah" });
+const item2 = new ItemModel({ name: "Öğle" });
+const item3 = new ItemModel({ name: "Akşam" });
 
-const defaultItems = [item1 , item2 , item3];
-
-ItemModel.insertMany(defaultItems).then(function(){
-    console.log("Success");
-}).catch(function(err){
-    console.log(err);
-});
-
+const defaultItems = [item1, item2, item3];
 
 
 app.get("/", function (req, res) {
 
-    const day = date.getDate();
-    res.render("list", { listTitle: day, newListItems:  items});
+    ItemModel.find().then( docs => {
+        if(docs.length ===0){
+
+            ItemModel.insertMany(defaultItems).then(function(){
+                console.log("Success");
+            }).catch( err=> {
+                console.log(err);
+            });
+            res.redirect("/");
+        } else {
+            const day = date.getDate();
+            res.render("list", { listTitle: day, newListItems: docs });
+        }
+    }).catch( err=> {
+        console.log(err);
+    });
+
 });
+
 
 app.post("/", function (req, res) {
 
-    const item = req.body.newItem;
-
     if (req.body.list === "Work List") {
-        workItems.push(item);
+        const workItem = new workItemModel({name :req.body.newItem
+        });
+        workItem.save();
         res.redirect("/work");
     } else {
-        items.push(item);
+        const item4 = new ItemModel({name :req.body.newItem});
+        item4.save();
         res.redirect("/");
     }
 });
 
 app.get("/work", function (req, res) {
 
-    res.render("list", { listTitle: "Work List", newListItems:  });
-});
-
-app.post("/work", function (req, res) {
-
-    const item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
+    workItemModel.find().then(workDocs => {
+        res.render("list", { listTitle: "Work List", newListItems: workDocs});
+    }).catch(err=> {
+        console.log(err);
+    }) 
 });
 
 app.get("/about", function (req, res) {
     res.render("about"); 
 });
-
-
 
 
 app.listen(3000 || process.env.POST, function () {
